3d7bdaca97a007fcbbb7ddc0608eb7c9bd7f0e75
Straya <gareth.barry511@gmail.com>
2025-07-19 16:42:14 +0800

    Added Config File

---
+++ config.py
@@ -0,0 +1,16 @@
+class Optimisation:
+    distance_prune = True
+    trim_html_file = True
+    cleanup_maps = True
+    folium_performance_mode = True
+    prescreen_coords = True
+
+class Map_Configuration:
+    altitude_map = False
+    circle_radius = 10
+    include_popups = False
+    low_signal_cutoff = 141
+
+    # Folium Map init
+    zoom = 11
+    location = (-34.63275333333333,116.12121166666665)
